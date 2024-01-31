d0f00568621682972c17ec3909b0135db27be6a4
Sunghan Shin <crossrunway01@gmail.com>
2024-01-31 09:12:12 +0900

    240130 Lecture

---
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -0,0 +1,23 @@
+package hello.core;
+
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+@Configuration
+@ComponentScan(
+        basePackages = "hello.core", // default는 해당 클래스를 포함하는 패키지
+        excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
+        // 기존 코드들을 최대한 활용하기 위해 이미 Bean에 등록한 것들은 제외하고 등록.
+        // 실무에서는 잘 쓰이지 않는 부분. 애초에 ComponentScan을 사용하기 때문
+)
+public class AutoAppConfig {
+
+    @Bean(name = "memoryMemberRepository")
+    MemberRepository memberRepository(){
+        return new MemoryMemberRepository();
+    }
+}
